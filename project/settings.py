b629f2308bb4cf2e24641a8c507cbfeb2eec7a0d
EsraaAta <112088404+EsraaAta@users.noreply.github.com>
2022-08-27 19:31:18 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -141,6 +141,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ataesraa168@gmail.com'
-EMAIL_HOST_PASSWORD = 'boonkmejbjnhsyzz'
+EMAIL_HOST_PASSWORD = 'key'
 EMAIL_USE_TLS = True
 EMAIL_PORT = '587'
